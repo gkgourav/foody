ed4bcbf0944f49dc5d4a440c40753df2131d9903
gkgourav <gkgouravkushwaha@gmail.com>
2020-10-04 17:21:33 +0530

    first commit

---
+++ static/js/ratings.js
@@ -0,0 +1,29 @@
+var a=2;
+            if(a==1){
+                document.getElementById('one').style.display = "block"
+            }
+            else if(a==2){
+                document.getElementById('one').style.display = "block"
+                document.getElementById('two').style.display = "block"
+            }
+            else if(a==3){
+                document.getElementById('one').style.display = "block"
+                document.getElementById('two').style.display = "block"
+                document.getElementById('three').style.display = "block"
+            }
+            else if(a==4){
+                document.getElementById('one').style.display = "block"
+                document.getElementById('two').style.display = "block"
+                document.getElementById('three').style.display = "block"
+                document.getElementById('four').style.display = "block"
+            }
+            else if(a==5){
+                document.getElementById('one').style.display = "block"
+                document.getElementById('two').style.display = "block"
+                document.getElementById('three').style.display = "block"
+                document.getElementById('four').style.display = "block"
+                document.getElementById('five').style.display = "block"
+            }
+            else{
+                document.getElementById('one').style.display = "none"
+            }
