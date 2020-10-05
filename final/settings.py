566ecb52fbdad43054554842c13991af97740f9f
gkgourav <gkgouravkushwaha@gmail.com>
2020-10-05 13:11:23 +0530

    first commit

--- final/settings.py
+++ final/settings.py
@@ -150,7 +150,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'streetfoodies20@gmail.com'
-EMAIL_HOST_PASSWORD = 'streetfullwithfoodies'
+EMAIL_HOST_PASSWORD = 'streetwalafoody'
 
 SITE_ID = 1
 # LOGIN_REDIRECT_URL = ‘/’
